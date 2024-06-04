479f4a5a7414c9fa804201c21a0124cf279bd23c
ShriraamNagarajan <rhb321john@gmail.com>
2024-06-04 19:50:58 +0800

    Created Domain Models and Added IdentityCore

---
+++ Finance_BlogPost/Models/Domain/BlogPostRejection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Finance_BlogPost.Models.Domain
+{
+	public class BlogPostRejection
+	{
+		public Guid Id { get; set; }
+		public string Reason { get; set; }
+
+		public Guid BlogPostID { get; set; }
+
+		[ForeignKey("BlogPostID")]
+		[ValidateNever]
+		public BlogPost BlogPost { get; set; }
+	}
+}

9a3ff1ba50fb8227b1802e3383f0297ede511b7b
ShriraamNagarajan <rhb321john@gmail.com>
2024-06-04 20:19:48 +0800

    Added Login and Registration Functionality

---
+++ Finance_BlogPost/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Finance_BlogPost.Models.ViewModels
+{
+	public class LoginViewModel
+	{
+		[Required]
+		public string Username { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+	
+	}
+}

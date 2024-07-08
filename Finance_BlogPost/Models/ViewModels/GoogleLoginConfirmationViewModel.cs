fe852f1ff09849185bc2a6ecafc21376781586cf
ShriraamNagarajan <rhb321john@gmail.com>
2024-07-08 14:12:05 +0800

    Added External Authentication (Google Login)

---
+++ Finance_BlogPost/Models/ViewModels/GoogleLoginConfirmationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Finance_BlogPost.Models.ViewModels
+{
+    public class GoogleLoginConfirmationViewModel
+    {
+        public string Email { get; set; }
+        [ValidateNever]
+        public string ReturnUrl { get; set; }
+    }
+}

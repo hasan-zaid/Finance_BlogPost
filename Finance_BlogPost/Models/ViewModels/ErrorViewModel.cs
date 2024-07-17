5fd44d9c86efe5f053cfaf23d55dd33986c90524
ShriraamNagarajan <rhb321john@gmail.com>
2024-07-17 11:27:36 +0800

    Added ErrorViewModel

---
+++ Finance_BlogPost/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Finance_BlogPost.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
